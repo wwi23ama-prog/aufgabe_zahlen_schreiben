8e12c0e092ef2d984172360cfb1127f75af8f810
Reiner Hüchting <dev@reinerhuechting.de>
2023-10-12 11:57:29 +0200

    Create function stubs.

---
+++ zahlen/zahlen.go
@@ -0,0 +1,22 @@
+package zahlen
+
+// Digits erwartet eine Zahl und liefert eine Liste mit deren Ziffern.
+func Digits(n int) []int {
+	result := []int{}
+	// TODO
+	return result
+}
+
+// DigitStrings erwartet eine Liste von Ziffern und liefert eine Liste mit deren Zeichenketten.
+func DigitStrings(digits []int) []string {
+	result := []string{}
+	// TODO
+	return result
+}
+
+// Compose erwartet eine Liste von Ziffer-Strings und liefert einen String mit der zusammengesetzten Zahl.
+func Compose(digits []string) string {
+	result := ""
+	// TODO
+	return result
+}
